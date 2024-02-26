c120dd3cad2c1e1da9d2deb05d517b6a15a7b62e
altxtech <andre.soares@altxtech.net>
2024-02-26 15:53:07 -0300

    Fixed which names should and shouldn't be exportable

--- src/database/database.go
+++ src/database/database.go
@@ -1,40 +1,44 @@
 package database
 
 import (
-	"github.com/google/uuid"
 	"errors"
 	"fmt"
+
+	"github.com/google/uuid"
 )
 
 type Configuration struct {
-	ID string `json:"id" firestore:"id"` // "" means unindentified configuration
+	ID        string `json:"id" firestore:"id"` // "" means unindentified configuration
 	ProjectID string `json:"project_id" firestore:"id"`
-	Dataset string `json:"dataset" firestore:"dataset"`
+	Dataset   string `json:"dataset" firestore:"dataset"`
 }
 
-func NewConfiguration( projectID string, dataset string) Configuration{
+func NewConfiguration(projectID string, dataset string) Configuration {
 	// Creates a new configuration without identity
-	return Configuration {
-		ID: "",
+	return Configuration{
+		ID:        "",
 		ProjectID: projectID,
-		Dataset: dataset,
+		Dataset:   dataset,
 	}
 }
 
 type Database interface {
-	InsertConfig(Configuration) (Configuration, error) 
+	InsertConfig(Configuration) (Configuration, error)
 	GetConfigByID(string) (Configuration, error) // Returns identified configuration
 }
 
-
-type InMemoryDatabase struct {
+type inMemoryDatabase struct {
 	Configurations map[string]Configuration
 }
 
-func (db *InMemoryDatabase) InsertConfig(c Configuration) (Configuration, error){
+func NewInMemoryDB() Database {
+	return &inMemoryDatabase{}
+}
 
-	var result Configuration 
-	
+func (db *inMemoryDatabase) InsertConfig(c Configuration) (Configuration, error) {
+
+	var result Configuration
+
 	// Check if configuration is unindentified
 	if c.ID != "" {
 		return result, errors.New("Can't insert identified config")
@@ -42,12 +46,12 @@ func (db *InMemoryDatabase) InsertConfig(c Configuration) (Configuration, error)
 
 	result = c
 	result.ID = uuid.NewString()
-	
+
 	db.Configurations[result.ID] = result
 	return result, nil
 }
 
-func (db *InMemoryDatabase) GetConfigByID(id string) (Configuration, error){
+func (db *inMemoryDatabase) GetConfigByID(id string) (Configuration, error) {
 	config, ok := db.Configurations[id]
 	if !ok {
 		return config, fmt.Errorf("Configuration with id %s not found", id)
